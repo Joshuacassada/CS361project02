51f25260d5624ed6af2df47543866639a34e48c4
Joshua Cassada <cassadjx@dukes.jmu.edu>
2024-10-29 17:46:32 -0400

    comnt

--- supervisor.c
+++ supervisor.c
@@ -45,6 +45,10 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
+    int semmode = S_IRUSR | S_IWUSR;
+    int semflg = O_CREAT | O_EXCL;
+
+
     pid_t pid = getpid();
     key_t shmkey = ftok(".", pid);    // Same key as used in sales.c for shared memory
     key_t msgkey = ftok(".", pid + 1); // Same key as used in sales.c for message queue
@@ -57,7 +61,7 @@ int main(int argc, char *argv[]) {
     msgid = Msgget(msgkey, 0666); // Connect to the existing message queue
 
     // Open the semaphore for synchronization with Sales
-    sem_rendezvous = Sem_open("/cassadjx_rendezvous_sem", 0);
+    sem_rendezvous = Sem_open("/cassadjx_rendezvous_sem", semflg, semmode, 0);
     if (sem_rendezvous == SEM_FAILED) {
         perror("sem_open /cassadjx_rendezvous_sem failed");
         exit(1);
@@ -68,7 +72,7 @@ int main(int argc, char *argv[]) {
 
     // Supervisor loop: wait for messages from factories
     while (sharedData->remain > 0) {
-        factoryMessage msg;
+        msgBuf msg;
         int result = Msgrcv(msgid, &msg, sizeof(msg) - sizeof(long), 0, 0);
         if (result == -1) {
             perror("Error receiving message");
@@ -80,26 +84,26 @@ int main(int argc, char *argv[]) {
         sharedData->remain -= parts_to_make;
         total_parts_made += parts_to_make;
 
-        printf("Factory #%d: Going to make %d parts in %d milliseconds\n", msg.factory_id, parts_to_make, msg.duration);
+        printf("Factory #%d: Going to make %d parts in %d milliseconds\n", msg.facID, parts_to_make, msg.duration);
         usleep(msg.duration * 1000); // Sleep for the specified duration (in milliseconds)
         iterations++;
 
         // Send production message
-        factoryMessage production_msg;
-        production_msg.mtype = msg.factory_id;
-        production_msg.factory_id = msg.factory_id;
-        production_msg.parts_made = parts_to_make;
+        msgBuf production_msg;
+        production_msg.mtype = msg.facID;
+        production_msg.facID = msg.facID;
+        production_msg.partsMade = parts_to_make;
         Msgsnd(msgid, &production_msg, sizeof(production_msg) - sizeof(long), 0);
 
-        printf("Factory #%d has made %d parts\n", msg.factory_id, parts_to_make);
+        printf("Factory #%d has made %d parts\n", msg.facID, parts_to_make);
     }
 
     // All parts made, send completion message
     for (int i = 1; i <= num_factories; i++) {
-        factoryMessage completion_msg;
+        msgBuf completion_msg;
         completion_msg.mtype = i;
-        completion_msg.factory_id = i;
-        completion_msg.parts_made = total_parts_made;
+        completion_msg.facID = i;
+        completion_msg.partsMade = total_parts_made;
         Msgsnd(msgid, &completion_msg, sizeof(completion_msg) - sizeof(long), 0);
     }
 
